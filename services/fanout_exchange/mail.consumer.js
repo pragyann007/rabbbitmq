c595fecf0355edd114043b35fd5aeb20305e1ab2
pragyann007 <pragyanthapaliya027@gmail.com>
2026-02-27 20:21:14 +0545

    mail consumerreaeddd andd notifcation started.

--- services/fanout_exchange/mail.consumer.js
+++ services/fanout_exchange/mail.consumer.js
@@ -1,1 +1,33 @@
 import amqp from "amqplib"
+
+const mail =async ()=>{
+
+    try {
+
+        const connection =await amqp.connect("amqp://localhost");
+        const channe = await amqp.createChannel();
+
+  const exchange = "create_exchange"
+      const exchange_type = "fanout"
+
+      await channe.assertExchange(exchange,exchange_type,{durable:false})
+
+      const quee = await channe.assertQuee('',{exclusive:true});
+
+      console.log(`waiting for message ======>>>`)
+
+
+      channe.consume(quee.quee,(msg)=>{
+        const automation = JSON.parse(msg.content.toString())
+        console.log("recieved msg ", automation)
+        channe.ack(msg)
+      })
+
+        
+    } catch (error) {
+
+        console.log(`error is ${error}`)
+        
+    }
+
+}
