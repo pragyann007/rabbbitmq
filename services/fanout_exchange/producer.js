38d0baaaf6b8454623174fb4a4e64597ee71edad
pragyann007 <pragyanthapaliya027@gmail.com>
2026-02-27 20:03:34 +0545

    producer created ...

--- services/fanout_exchange/producer.js
+++ services/fanout_exchange/producer.js
@@ -1,3 +1,32 @@
 import {amqp} from "amqplib"
 
-const lk = __dirname;
+const automationCreated = async (automation)=>{
+  try {
+      const connectioons = await amqp.connect("amqp://localhost")
+      const channel = await connectioons.createChannel();
+  
+      const exchange = "create_exchange"
+      const exchange_type = "fanout"
+  
+      await channel.assertExchange(exchange,exchange_type,{durable:false})
+  
+      const message = JSON.stringify(automation);
+  
+      channel.publish(exchange,"",Buffer.from(message),{persistent:false});
+      console.log("sent message =====>>>>> ");
+
+      setTimeout(() => {
+
+        connectioons.close();
+        
+      }, 500);
+  } catch (error) {
+
+    console.log(`error iss ${error}`)
+    
+  }
+
+
+}
+
+automationCreated();
